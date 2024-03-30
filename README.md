ca846c033fa64578d5c3cafaa04e15b07dc521d8
Olabode Aguda <olabodeaguda@Outlook.com>
2024-03-30 01:41:09 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Onnex Admin portal
+
+    This App is build for Onnex internal users for operations and supports.
+
+## Modules
+
+    - Api management
+    - User management
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (  
+    <div className="bg-slate-200">
+       <h1 className="text-5xl text-green-300 font-bold underline bg-slate-800">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
